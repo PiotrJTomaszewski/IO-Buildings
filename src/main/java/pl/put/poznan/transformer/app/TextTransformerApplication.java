f04fc7b120dfb08b8821e0dd975495bf8f5c17dc
Piotr J. Tomaszewski <pjtomaszewski98@gmail.com>
2019-10-29 21:52:59 +0100

    Notification test

--- src/main/java/pl/put/poznan/transformer/app/TextTransformerApplication.java
+++ src/main/java/pl/put/poznan/transformer/app/TextTransformerApplication.java
@@ -1,7 +1,7 @@
 package pl.put.poznan.transformer.app;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+iamport org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication(scanBasePackages = {"pl.put.poznan.transformer.rest"})
