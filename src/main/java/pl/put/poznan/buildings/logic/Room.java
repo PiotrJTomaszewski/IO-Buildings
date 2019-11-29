f5aafb8f35b785997a3fe0ef664cc312f0ab50f2
Sebastian <sebastian.michon10@protonmail.com>
2019-11-29 18:04:11 +0100

    Zbiór pomieszczeń zużywających energię>threshold

--- src/main/java/pl/put/poznan/buildings/logic/Room.java
+++ src/main/java/pl/put/poznan/buildings/logic/Room.java
@@ -1,4 +1,7 @@
 package pl.put.poznan.buildings.logic;
+import java.util.ArrayList;
+
+import com.sun.tools.javac.util.List;
 
 public class Room extends Location {
     private int area;
@@ -7,14 +10,24 @@ public class Room extends Location {
     private int light;
     
     /**
-     * Gives energy used by certain room
+     * Gives heating energy used by a certain room
      * 
-     * @return energy used by a room
+     * @return heating energy used by a room
+     * @author sebastian_michon
+     */
+    public float getHeatingEnergyUse() {
+    	return this.heating/this.cube;
+    }
+    
+    /** Gives whole energy used by a certain room
+     * 
+     * @return whole energy used by a room
      * @author sebastian_michon
      */
     public float getEnergyUse() {
-    	return this.heating/this.cube;
+    	return this.getHeatingEnergyUse();
     }
+    
     
     /**
      * Calculates the area of the room
@@ -24,15 +37,34 @@ public class Room extends Location {
     	return this.area;
     }
     
+    /**
+     * Calculates heating of the room
+     * @return Heating value of the room
+     */
     public float getHeating() {
     	return this.heating;
     }
+    
     /**
      * Calculates the volume of the room
      * @return The volume of the room
      */
     public int getCube() {
     	return this.cube;
+    }
+    
+    /**
+     * calculates, whether energy used by room is greater than threshold
+     * 
+     * @param thr - threshold, if building uses more energy than threshold
+     * @return Location if room uses more energy than threshold or null otherwise
+     */
+    public ArrayList<Location> thresholding_energy(float thr) {
+    	ArrayList<Location> isthresholded = new ArrayList<>();
+    	if (this.getEnergyUse()>thr) {
+    		isthresholded.add((Location)this);
+    	}
+    	return isthresholded;
     }
     
 }
