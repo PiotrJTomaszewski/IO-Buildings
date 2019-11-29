0b6908c603269f6fe3a97d9dca98fc7955d07362
Piotr J. Tomaszewski <pjtomaszewski98@gmail.com>
2019-11-29 16:02:58 +0100

    Liczenie powierzchni

--- src/main/java/pl/put/poznan/buildings/logic/Location.java
+++ src/main/java/pl/put/poznan/buildings/logic/Location.java
@@ -23,6 +23,8 @@ public abstract class Location {
     public void setName(String name) {
         this.name = name;
     }
+    
+    public abstract int getArea();
 
     @Override
     public String toString() {
