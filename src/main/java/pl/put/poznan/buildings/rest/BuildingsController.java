f96205c2d82837352c966f01d8a20803eff8a041
Marcin Zatorski <mar3211233@gmail.com>
2019-11-29 16:35:35 +0100

    Add endpoint for rooms and levels

--- src/main/java/pl/put/poznan/buildings/rest/BuildingsController.java
+++ src/main/java/pl/put/poznan/buildings/rest/BuildingsController.java
@@ -10,9 +10,29 @@ public class BuildingsController {
 
     private static final Logger logger = LoggerFactory.getLogger(BuildingsController.class);
 
-    @PostMapping("/buildings")
-    public Building postBuilding(@RequestBody Building building) {
+    @PostMapping("/buildings/{buildingId}")
+    public Building postBuilding(@PathVariable long buildingId, @RequestBody Building building) {
         // log the parameters
+        logger.debug(Long.toString(buildingId));
+        logger.debug(building.toString());
+        return building;
+    }
+
+    @PostMapping("/buildings/{buildingId}/levels/{levelId}")
+    public Building postBuilding(@PathVariable long buildingId, @PathVariable long levelId, @RequestBody Building building) {
+        // log the parameters
+        logger.debug(Long.toString(buildingId));
+        logger.debug(Long.toString(levelId));
+        logger.debug(building.toString());
+        return building;
+    }
+
+    @PostMapping("/buildings/{buildingId}/levels/{levelId}/rooms/{roomId}")
+    public Building postBuilding(@PathVariable long buildingId, @PathVariable long levelId, @PathVariable long roomId, @RequestBody Building building) {
+        // log the parameters
+        logger.debug(Long.toString(buildingId));
+        logger.debug(Long.toString(levelId));
+        logger.debug(Long.toString(roomId));
         logger.debug(building.toString());
         return building;
     }
