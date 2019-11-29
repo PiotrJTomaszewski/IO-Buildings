d466c886bf3fef8fbc73653640fd500fa4e5ba10
Marcin Zatorski <mar3211233@gmail.com>
2019-11-30 00:55:46 +0100

    Change api endpoints
     - Merge all endpoints into /locations
     - Add endpoints for location properties

--- src/main/java/pl/put/poznan/buildings/rest/BuildingsController.java
+++ src/main/java/pl/put/poznan/buildings/rest/BuildingsController.java
@@ -3,6 +3,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import pl.put.poznan.buildings.logic.Building;
+import pl.put.poznan.buildings.logic.Location;
+import pl.put.poznan.buildings.logic.Room;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 @RestController
@@ -10,32 +16,58 @@ public class BuildingsController {
 
     private static final Logger logger = LoggerFactory.getLogger(BuildingsController.class);
 
-    @PostMapping("/buildings/{buildingId}")
-    public Building postBuilding(@PathVariable long buildingId, @RequestBody Building building) {
+    @PostMapping("/locations/{id}")
+    public Building postLocation(@PathVariable long id, @RequestBody Building building) {
         // log the parameters
-        logger.debug(Long.toString(buildingId));
+        logger.debug(Long.toString(id));
         logger.debug(building.toString());
         return building;
     }
 
-    @PostMapping("/buildings/{buildingId}/levels/{levelId}")
-    public Building postBuilding(@PathVariable long buildingId, @PathVariable long levelId, @RequestBody Building building) {
+    @PostMapping(path = "/locations/{id}/area")
+    public Map<String, Integer> postLocationArea(@PathVariable long id, @RequestBody Building building) {
         // log the parameters
-        logger.debug(Long.toString(buildingId));
-        logger.debug(Long.toString(levelId));
+        logger.debug(Long.toString(id));
         logger.debug(building.toString());
-        return building;
+        return toKeyValue("area", building.getArea());
     }
 
-    @PostMapping("/buildings/{buildingId}/levels/{levelId}/rooms/{roomId}")
-    public Building postBuilding(@PathVariable long buildingId, @PathVariable long levelId, @PathVariable long roomId, @RequestBody Building building) {
+    @PostMapping(path = "/locations/{id}/cube")
+    public Map<String, Integer> postLocationCube(@PathVariable long id, @RequestBody Building building) {
         // log the parameters
-        logger.debug(Long.toString(buildingId));
-        logger.debug(Long.toString(levelId));
-        logger.debug(Long.toString(roomId));
+        logger.debug(Long.toString(id));
         logger.debug(building.toString());
-        return building;
+        return toKeyValue("cube", building.getCube());
+    }
+
+    @PostMapping(path = "/locations/{id}/light")
+    public Map<String, Float> postLocationLight(@PathVariable long id, @RequestBody Building building) {
+        // log the parameters
+        logger.debug(Long.toString(id));
+        logger.debug(building.toString());
+        return toKeyValue("light", building.getMeanLight());
+    }
+
+    @PostMapping(path = "/locations/{id}/heating")
+    public Map<String, Float> postLocationHeating(@PathVariable long id, @RequestBody Building building) {
+        // log the parameters
+        logger.debug(Long.toString(id));
+        logger.debug(building.toString());
+        return toKeyValue("heating", building.getHeatingEnergyUse());
+    }
+
+    @PostMapping(path = "/locations/{id}/exceed")
+    public List<Location> postLocationSuboptimal(@PathVariable long id, @RequestParam float heatingEnergy, @RequestBody Building building) {
+        // log the parameters
+        logger.debug(Long.toString(id));
+        logger.debug(Float.toString(heatingEnergy));
+        logger.debug(building.toString());
+        return building.thresholding_energy(heatingEnergy);
+    }
+
+    private <T> Map<String, T> toKeyValue(String key, T value) {
+        Map<String, T> map = new HashMap<>();
+        map.put(key, value);
+        return map;
     }
 }
-
-
