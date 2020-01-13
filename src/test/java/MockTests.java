1a2f9f6fc6fb308101ed7b39ffa48b870cb88c9d
Sebastian <sebastian.michon10@protonmail.com>
2020-01-13 20:32:23 +0100

    10 abhorrent, ridiculous and working mock tests - perhaps I'll change them yet today

--- src/test/java/MockTests.java
+++ src/test/java/MockTests.java
@@ -1,7 +1,9 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.HashMap;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.opentest4j.AssertionFailedError;
 
 import pl.put.poznan.buildings.logic.Building;
 import pl.put.poznan.buildings.logic.Floor;
@@ -9,71 +11,184 @@ import pl.put.poznan.buildings.logic.Location;
 import pl.put.poznan.buildings.logic.LocationVisitor;
 import pl.put.poznan.buildings.logic.Room;
 import pl.put.poznan.buildings.rest.BuildingsController;
-
+import pl.put.poznan.buildings.rest.LocationNotFoundException;
 
 import static org.mockito.Mockito.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 class MockTests {
 	
-	private Building x;
-	private Room a, b, c, d;
-
-	@BeforeEach
-	void setUp() throws Exception {
-		x=new Building();
-		
-		a=new Room();
-		b=new Room();
-		c=new Room();
-		d=new Room();
-		
-		a.setArea(20);
-		b.setArea(310);
-		c.setArea(12000);
-		d.setArea(5);
-		
-		a.setCube(100);
-		b.setCube(30000);
-		c.setCube(350);
-		d.setCube(120);
-		
-		a.setHeating(100.5f);
-		b.setHeating(1200.55f);
-		c.setHeating(0);
-		d.setHeating(400);
-		
-		a.setLight(20);
-		b.setLight(620);
-		c.setLight(1);
-		d.setLight(400);
-		
-		Floor e=new Floor();
-		List<Location> z=new ArrayList<Location>();
-		z.add(a);
-		z.add(b);
-		e.setLocations(z);
-		
-		List<Location> y=new ArrayList<Location>();
-		y.add(e);
-		y.add(c);
-		y.add(d);
-		
-		x.setLocations(y);
-		
-	}
+	private <T> Map<String, T> toKeyValue(String key, T value) {
+        Map<String, T> map = new HashMap<>();
+        map.put(key, value);
+        return map;
+    }
 	
 	@Test
-	void BuildingTest() {
+	void PostLocationNoErrorTest() {
 		Building k=mock(Building.class);
 		when(k.getId()).thenReturn((long) 13);
 		List<Location> c=new ArrayList<Location>();
 		c.add(k);
 		when(k.getLocations()).thenReturn(c);
 		doCallRealMethod().when(k).accept(any());
-		
 		BuildingsController bc=new BuildingsController();
 		bc.postLocation(13, k);
+		assertDoesNotThrow(()->{
+			bc.postLocation(13, k);
+		});
 	}
+	
+	@Test
+	void PostLocationErrorTest() {
+		Building k=mock(Building.class);
+		when(k.getId()).thenReturn((long) 17);
+		List<Location> c=new ArrayList<Location>();
+		c.add(k);
+		when(k.getLocations()).thenReturn(c);
+		doCallRealMethod().when(k).accept(any());
+		BuildingsController bc=new BuildingsController();
+		assertThrows(StackOverflowError.class, ()->{
+			bc.postLocation(14, k);
+		});
+	}
+	
+	@Test
+	void PostLocationHeatingTest() {
+		Building k=mock(Building.class);
+		when(k.getId()).thenReturn((long) 13);
+		List<Location> c=new ArrayList<Location>();
+		c.add(k);
+		when(k.getLocations()).thenReturn(c);
+		doCallRealMethod().when(k).accept(any());
+		//doCallRealMethod().when(k).getHeatingEnergyUse();
+		
+		BuildingsController bc=new BuildingsController();
+		assertDoesNotThrow(()->{
+			Map<String, Float> m=bc.postLocationHeating(13, k);
+			assertEquals(m, toKeyValue("heating", 0.0f));
+		});
+	}
+	
+	@Test
+	void PostLocationHeatingErrorTest() {
+		Building k=mock(Building.class);
+		when(k.getId()).thenReturn((long) 13);
+		List<Location> c=new ArrayList<Location>();
+		c.add(k);
+		when(k.getLocations()).thenReturn(c);
+		doCallRealMethod().when(k).accept(any());
+		doCallRealMethod().when(k).getHeatingEnergyUse();
+		
+		BuildingsController bc=new BuildingsController();
+		assertThrows(AssertionFailedError.class, ()->{
+			Map<String, Float> m=bc.postLocationHeating(13, k);
+			assertEquals(m, toKeyValue("heating", 0.0f));
+		});
+	}
+	
+	@Test
+	void PostLocationLightTest() {
+		Building k=mock(Building.class);
+		when(k.getId()).thenReturn((long) 13);
+		List<Location> c=new ArrayList<Location>();
+		c.add(k);
+		when(k.getLocations()).thenReturn(c);
+		doCallRealMethod().when(k).accept(any());
+		//doCallRealMethod().when(k).getHeatingEnergyUse();
+		
+		BuildingsController bc=new BuildingsController();
+		assertDoesNotThrow(()->{
+			Map<String, Float> m=bc.postLocationLight(13, k);
+			assertEquals(m, toKeyValue("light", 0.0f));
+		});
+	}
+	
+	@Test
+	void PostLocationLightErrorTest() {
+		Building k=mock(Building.class);
+		when(k.getId()).thenReturn((long) 13);
+		List<Location> c=new ArrayList<Location>();
+		c.add(k);
+		when(k.getLocations()).thenReturn(c);
+		doCallRealMethod().when(k).accept(any());
+		doCallRealMethod().when(k).getMeanLight();
+		
+		BuildingsController bc=new BuildingsController();
+		assertThrows(AssertionFailedError.class, ()->{
+			Map<String, Float> m=bc.postLocationLight(13, k);
+			assertEquals(m, toKeyValue("light", 0.0f));
+		});
+	}
+	
+	@Test
+	void PostLocationAreaTest() {
+		Building k=mock(Building.class);
+		when(k.getId()).thenReturn((long) 13);
+		List<Location> c=new ArrayList<Location>();
+		c.add(k);
+		when(k.getLocations()).thenReturn(c);
+		doCallRealMethod().when(k).accept(any());
+		//doCallRealMethod().when(k).getHeatingEnergyUse();
+		
+		BuildingsController bc=new BuildingsController();
+		assertDoesNotThrow(()->{
+			Map<String, Integer> m=bc.postLocationArea(13, k);
+			assertEquals(m, toKeyValue("area", 0));
+		});
+	}
+	
+	@Test
+	void PostLocationAreaErrorTest() {
+		Building k=mock(Building.class);
+		when(k.getId()).thenReturn((long) 13);
+		List<Location> c=new ArrayList<Location>();
+		c.add(k);
+		when(k.getLocations()).thenReturn(c);
+		doCallRealMethod().when(k).accept(any());
+		doCallRealMethod().when(k).getArea();
+		
+		BuildingsController bc=new BuildingsController();
+		assertThrows(NullPointerException.class, ()->{
+			Map<String, Integer> m=bc.postLocationArea(13, k);
+			assertEquals(m, toKeyValue("area", 0));
+		});
+	}
+	
+	@Test
+	void PostLocationCubeTest() {
+		Building k=mock(Building.class);
+		when(k.getId()).thenReturn((long) 13);
+		List<Location> c=new ArrayList<Location>();
+		c.add(k);
+		when(k.getLocations()).thenReturn(c);
+		doCallRealMethod().when(k).accept(any());
+		//doCallRealMethod().when(k).getHeatingEnergyUse();
+		
+		BuildingsController bc=new BuildingsController();
+		assertDoesNotThrow(()->{
+			Map<String, Integer> m=bc.postLocationCube(13, k);
+			assertEquals(m, toKeyValue("cube", 0));
+		});
+	}
+	
+	@Test
+	void PostLocationCubeErrorTest() {
+		Building k=mock(Building.class);
+		when(k.getId()).thenReturn((long) 13);
+		List<Location> c=new ArrayList<Location>();
+		c.add(k);
+		when(k.getLocations()).thenReturn(c);
+		doCallRealMethod().when(k).accept(any());
+		doCallRealMethod().when(k).getCube();
+		
+		BuildingsController bc=new BuildingsController();
+		assertThrows(NullPointerException.class, ()->{
+			Map<String, Integer> m=bc.postLocationCube(13, k);
+			assertEquals(m, toKeyValue("cube", 0));
+		});
+	}
+	
 }
